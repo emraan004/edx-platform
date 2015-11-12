62f69918b872748708f48eb17fd308a1c71d8478
Afeef Janjua <janjua.afeef@gmail.com>
2015-11-12 11:38:01 +0500

    organized courses on student dashboard using backbone tabs

---
+++ lms/static/js/dashboard/collection/courses.js
@@ -0,0 +1,10 @@
+;(function (define) {
+    'use strict';
+
+    define(['backbone'], function (Backbone) {
+        return Backbone.Collection.extend({
+
+        });
+    });
+
+}).call(this, define || RequireJS.define);
